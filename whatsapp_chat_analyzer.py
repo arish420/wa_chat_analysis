559cbb0e34a9bb2345e5954f9e11f06961e340a4
arish420 <muhammadarish420@gmail.com>
2025-07-03 09:15:51 +0500

    Update whatsapp_chat_analyzer.py

--- whatsapp_chat_analyzer.py
+++ whatsapp_chat_analyzer.py
@@ -294,15 +294,15 @@ def preprocessing(uploaded_file):
 	message_split.columns=['User','Message']
 	frames=[into_df,message_split]
 	df=pd.concat(frames,axis=1)
-	df['Date and Time']=pd.to_datetime(df['Date and Time'])
-	df['Year']=df['Date and Time'].dt.year
-	df['Month']=df['Date and Time'].dt.month_name()
-	df['Day']=df['Date and Time'].dt.day
-	df['Hour']=df['Date and Time'].dt.hour
-	df['Minutes']=df['Date and Time'].dt.minute
-	df['Month No']=df['Date and Time'].dt.month
-	df['Date']=df['Date and Time'].dt.date
-	df['Day']=df['Date and Time'].dt.day_name()
+	# df['Date and Time']=pd.to_datetime(df['Date and Time'])
+	# df['Year']=df['Date and Time'].dt.year
+	# df['Month']=df['Date and Time'].dt.month_name()
+	# df['Day']=df['Date and Time'].dt.day
+	# df['Hour']=df['Date and Time'].dt.hour
+	# df['Minutes']=df['Date and Time'].dt.minute
+	# df['Month No']=df['Date and Time'].dt.month
+	# df['Date']=df['Date and Time'].dt.date
+	# df['Day']=df['Date and Time'].dt.day_name()
 	return df
 #application title
 slt.title("Whatsapp Chat Analyzer")
